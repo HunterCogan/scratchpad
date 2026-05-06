998bc587ba91cd9b62062fafec963835790dc9a5
Hunter Cogan <robert.h.cogan@gmail.com>
2026-05-06 15:18:02 -0400

    Install HeroUI and show an example button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client"
+
+import { Button } from "@heroui/react";
+import { CalculatorIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 export default function Home() {
+  let [exampleNumber, setExampleNumber] = useState(0)
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center py-32 px-16 sm:items-start">
         <h1 className="text-4xl">
           Scratchpad
         </h1>
         
-        <p className="mt-4">
+        <p className="mt-6">
           This is an example for the landing page of Scratchpad. We will make much more in the future.
         </p>
+        
+        <Button
+          className={"mt-6"}
+          onPress={() => setExampleNumber((prevNum) => prevNum + 1)}
+        >
+          <CalculatorIcon /> Example HeroUI button: {exampleNumber}
+        </Button>
       </main>
     </div>
   );
