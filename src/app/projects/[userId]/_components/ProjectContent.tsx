8f897a495ca851ebe3dfc010fc49e84f1b31c185
Adonis Canada <adonis.a.canada@gmail.com>
2026-05-25 19:25:05 -0400

    Download button functionality, remove metadata from uploaded project.json

--- src/app/projects/[userId]/_components/ProjectContent.tsx
+++ src/app/projects/[userId]/_components/ProjectContent.tsx
@@ -128,7 +128,24 @@ export function ProjectContent({ remixes }: Props) {
             </ScrollShadow>
           </Card.Content>
           <Card.Footer className="flex gap-2">
-            <Button size="sm">
+            <Button
+              size="sm"
+              onPress={() => {
+                if (!selectedRemix) return;
+                // Store project.json of selectedRemix as raw binary data
+                // then create a URL to point to this data to download it
+                const blob = new Blob([selectedRemix.projectJsonData], {
+                  type: "application/json",
+                });
+                const url = URL.createObjectURL(blob);
+                // link element with "download" required for download
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "project.json";
+                a.click();
+                URL.revokeObjectURL(url);
+              }}
+            >
               <ArrowDownTrayIcon />
               Download
             </Button>
